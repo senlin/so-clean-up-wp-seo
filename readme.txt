0cfcb299bf5223b0ecbbc72875c00f1b13a9f598
Piet Bos <info@senlinonline.com>
2015-04-27 14:15:47 +0800

    commit version 1.1

---
+++ readme.txt
@@ -0,0 +1,82 @@
+=== SO Clean Up WP SEO ===
+Contributors: senlin
+Donate link: http://so-wp.com/donations
+Tags: wordpress seo, yoast, seo, remove, disable, about, tour, sidebar, ads, dashboard, widget, columns 
+Requires at least: 4.0
+Tested up to: 4.2
+Stable tag: 1.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Clean up several things that the WordPress SEO plugin adds to your WordPress Dashboard
+
+== Description ==
+
+Almost anyone that uses WordPress SEO by Team Yoast will agree that it is *the* SEO plugin, but the developers are adding more and more unwanted things to the WordPress Dashboard.
+
+The purpose of the SO Clean Up WP SEO is to clean up all those unwanted things.
+
+As per the current release, the plugin removes and/or disables the following unwanted items:
+
+* globally sets plugin-tour to done and plugin about-page as seen
+* removes sidebar ads on all WordPress SEO settings pages
+* removes the WordPress SEO dashboard widget
+* removes the keyword/description columns on edit Posts/Pages pages
+
+We support this plugin exclusively through [Github](https://github.com/senlin/so-clean-up-wp-seo/issues). Therefore, if you have any questions, need help and/or want to make a feature request, please open an issue over at Github. You can also browse through open and closed issues to find what you are looking for and perhaps even help others.
+
+**PLEASE DO NOT POST YOUR ISSUES VIA THE WORDPRESS FORUMS**
+
+Thanks for your understanding and cooperation.
+
+If you like the SO Clean Up WP SEO plugin, please consider leaving a [review](http://wordpress.org/support/view/plugin-reviews/so-clean-up-wp-seo#postform) or making a [donation](http://so-wp.com/donations/). Thanks!
+
+
+== Installation ==
+
+= Wordpress =
+
+Search for "so clean up wp seo" and install with the **Plugins > Add New** back-end page.
+
+ &hellip; OR &hellip;
+
+Follow these steps:
+
+ 1. Download zip file.
+
+ 2. Upload the zip file via the Plugins > Add New > Upload page &hellip; OR &hellip; unpack and upload with your favourite FTP client to the /plugins/ folder.
+
+ 3. Activate the plugin on the Plugins page.
+
+Done!
+
+
+== Frequently Asked Questions ==
+
+= Where is the settings page? =
+
+You can stop looking, there is none. Activate the plugin to clean up your WordPress dashboard.
+
+= I have an issue with this plugin, where can I get support? =
+
+Please open an issue on [Github](https://github.com/senlin/so-clean-up-wp-seo/issues)
+
+== Screenshots ==
+
+N/A
+
+== Changelog ==
+
+= 1.1 =
+
+* Release version (2015.04.27)
+* banner image (in assets folder) by [Leigh Kendell](https://unsplash.com/leighkendell)
+
+= 1.0 =
+
+* Initial plugin [code snippet](https://github.com/senlin/Code-Snippets/blob/0ae24e6fc069efe26e52007c05c7375012ee688a/Functions/Admin-Dashboard/remove-yoast-crap.php) (2015.04.24)
+
+== Upgrade Notice ==
+
+N/A
+
---
+++ so-clean-up-wp-seo.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Plugin Name: SO Clean Up WP SEO
+ * Plugin URI:  http://so-wp.com/plugin/so-clean-up-wp-seo/
+ * Description: Clean up several things that the WordPress SEO plugin adds to your WordPress Dashboard
+ * Author:      SO WP
+ * Author URI:  http://so-wp.com/plugins/
+ * Version:     1.1
+ * License:     GPL3+
+ */
+
+/**
+ * Prevent direct access to files
+ *
+ * @since 1.0
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Version check; any WP version under 4.0 is not supported (if only to "force" users to stay up to date)
+ * 
+ * adapted from example by Thomas Scholz (@toscho) http://wordpress.stackexchange.com/a/95183/2015, Version: 2013.03.31, Licence: MIT (http://opensource.org/licenses/MIT)
+ *
+ * @since 1.1
+ */
+
+//Only do this when on the Plugins page.
+if ( ! empty ( $GLOBALS['pagenow'] ) && 'plugins.php' === $GLOBALS['pagenow'] )
+	
+	/* so_cuws_ prefix is derived from [so] [c]lean [u]p [w]p [s]eo. */
+	add_action( 'admin_notices', 'so_cuws_check_admin_notices', 0 );
+
+function so_cuws_min_wp_version() {
+	global $wp_version;
+	$require_wp = '4.0';
+	$update_url = get_admin_url( null, 'update-core.php' );
+
+	$errors = array();
+
+	if ( version_compare( $wp_version, $require_wp, '<' ) ) 
+
+		$errors[] = "You have WordPress version $wp_version installed, but <b>this plugin requires at least WordPress $require_wp</b>. Please <a href='$update_url'>update your WordPress version</a>.";
+
+	return $errors; 
+}
+
+function so_cuws_check_admin_notices() {
+	
+	$errors = so_cuws_min_wp_version();
+
+	if ( empty ( $errors ) )
+		return;
+
+	// Suppress "Plugin activated" notice.
+	unset( $_GET['activate'] );
+
+	// this plugin's name
+	$name = get_file_data( __FILE__, array ( 'Plugin Name' ), 'plugin' );
+
+	printf( __( '<div class="error"><p>%1$s</p><p><i>%2$s</i> has been deactivated.</p></div>', 'so-clean-up-wp-seo' ),
+		join( '</p><p>', $errors ),
+		$name[0]
+	);
+	deactivate_plugins( plugin_basename( __FILE__ ) );
+
+}
+
+/**
+ * This function checks whether the WordPress SEO plugin is active (it needs to be active for SO Clean Up WP SEO to have any use)
+ *
+ * @since 1.1
+ */
+
+$plugins = get_option( 'active_plugins' );
+
+$required_plugin = 'wordpress-seo/wp-seo.php';
+
+// multisite throws the error message by default, because the plugin is installed on the network site, therefore check for multisite
+if ( ! in_array( $required_plugin , $plugins ) && ! is_multisite() ) {
+
+	add_action( 'admin_notices', 'so_cuws_warning' );
+
+}
+
+/**
+ * Show warning if the WordPress SEO plugin has not been installed
+ *
+ * @since 1.1
+ */
+
+function so_cuws_warning() {
+    
+    // display the warning message
+    echo '<div class="message error"><p>';
+    
+    _e( 'The <strong>SO Clean Up WP SEO plugin</strong> only works if you have the WordPress SEO plugin installed.', 'so-clean-up-wp-seo' );
+    
+    echo '</p></div>';
+    
+}
+
+/**
+ * if the WordPress SEO plugin has been installed, add the filters and actions that clean up the entire WP SEO experience
+ *
+ * @since 1.0
+ */
+
+
+if ( in_array( $required_plugin , $plugins ) ) {
+	
+	add_filter( 'option_wpseo', 'so_cuws_remove_about_tour' );
+
+	add_action( 'admin_head', 'so_cuws_hide_sidebar_ads' );	
+	
+	add_action( 'admin_menu', 'so_cuws_remove_dashboard_widget' );
+	
+	if ( function_exists( 'wpseo_use_page_analysis' ) ) {
+		add_filter( 'wpseo_use_page_analysis', '__return_false' );
+	}
+
+}
+
+/**
+ * After each update of the Yoast WordPress SEO plugin, the user is redirected
+ * to the About page of the plugin (admin.php?page=wpseo_dashboard&intro=1#top#new)
+ *
+ * This is irritating at best and very unprofessional on websites of (large) companies and organisations
+ * with many users that have the Administrator Role.
+ *
+ * This filter globally sets the "see about page" setting and the "ignore tour" setting to true
+ *
+ * Drop this file into the wp-content/mu-plugins folder (create it if it doesn't exist) and you and your fellow Admins
+ * will not be disturbed anymore by this nonsense.
+ *
+ * @source: github.com/Yoast/wordpress-seo/pull/2235#issuecomment-95059096
+ */
+function so_cuws_remove_about_tour( $option ) {
+
+	$option['seen_about'] = true;
+	$option['ignore_tour'] = true;
+
+	return $option;
+
+}
+
+// remove WordPress SEO dashboard widget
+function so_cuws_remove_dashboard_widget() {
+
+	remove_meta_box( 'yoast_db_widget', 'dashboard', 'normal' ); // Yoast's WordPress SEO Plugin Widget
+
+}
+
+// Remove irritating adds sidebar
+function so_cuws_hide_sidebar_ads() {
+	echo '<style type="text/css">
+	#sidebar-container.wpseo_content_cell {display:none;}
+	</style>';
+}
