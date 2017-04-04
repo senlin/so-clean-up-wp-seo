eae78fab07315da431755a0a9f2032343b3804fc
Andy Fragen <andy@thefragens.com>
2017-04-03 18:15:45 -0700

    Fix uninstall to use `$wpdb->prepare()`
    Necessary for potential security issues.

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 
+ *
  * This file runs when the plugin in uninstalled (deleted).
  * This will not run when the plugin is deactivated.
  * Ideally you will add all your clean-up scripts here
@@ -22,6 +22,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
  * @since v2.0.0
  */
 global $wpdb;
-$wpdb->query( "DELETE FROM `{$wpdb->prefix}options` WHERE `option_name` LIKE '%cuws_%'" );
-
-
+$table         = is_multisite() ? $wpdb->base_prefix . 'sitemeta' : $wpdb->base_prefix . 'options';
+$column        = is_multisite() ? 'meta_key' : 'option_name';
+$delete_string = 'DELETE FROM ' . $table . ' WHERE ' . $column . ' LIKE %s LIMIT 1000';
+$wpdb->query( $wpdb->prepare( $delete_string, array( '%cuws_%' ) ) );
