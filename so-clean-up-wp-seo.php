4ed560090dc0481d4062199f0e01f45d5e1fb48d
Piet Bos <piet@so-wp.com>
2017-04-06 22:51:27 +0000

    add plugin's location on Github
    make plugin suitable for use with Github Updater by @afragen

--- so-clean-up-wp-seo.php
+++ so-clean-up-wp-seo.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Plugin Name: SO Hide SEO Bloat
- * Plugin URI:  https://so-wp.com/plugin/so-clean-up-wp-seo/
- * Description: Hide most of the bloat that the Yoast SEO plugin adds to your WordPress Dashboard
- * Version:     2.6.1
- * Author:      SO WP
- * Author URI:  https://so-wp.com/plugins/
- * Text Domain: so-clean-up-wp-seo
- * Domain Path: /languages
- * License:     GPL-3.0+
- * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
- * Network:     true
+ * Plugin Name: 		SO Hide SEO Bloat
+ * Plugin URI:  		https://so-wp.com/plugin/so-clean-up-wp-seo/ * GitHub Plugin URI: senlin/so-clean-up-wp-seo
+ * Description:			Hide most of the bloat that the Yoast SEO plugin adds to your WordPress Dashboard
+ * Version:     		2.6.1
+ * Author:				SO WP
+ * Author URI:  		https://so-wp.com/plugins/
+ * License:    			GPL-3.0+
+ * License URI:			http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: 		/languages
+ * Text Domain: 		so-clean-up-wp-seo
+ * Network:     		true
+ * GitHub Plugin URI:	https://github.com/senlin/so-clean-up-wp-seo
+ * GitHub Branch:		master
  */
 
 // don't load the plugin file directly
