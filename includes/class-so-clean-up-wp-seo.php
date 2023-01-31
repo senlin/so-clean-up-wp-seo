3c535418f46350aacad34186a972cfd1b9ec7aaa
Pieter Bos <pieter@so-wp.com>
2023-01-31 12:51:55 +0000

    order submenu items has changed
    see https://wordpress.org/support/topic/last-submenu-premium-still-appears/

--- includes/class-so-clean-up-wp-seo.php
+++ includes/class-so-clean-up-wp-seo.php
@@ -388,7 +388,7 @@ class CUWS {
 		// hide "Premium" submenu in its entirety
 		// include hiding "Workouts" submenu in its entirety 
 		if ( ! empty( $this->options['hide_premium_submenu'] ) ) {
-			echo 'li#toplevel_page_wpseo_dashboard>ul>li:nth-child(7),li#toplevel_page_wpseo_dashboard>ul>li:nth-child(8),li#toplevel_page_wpseo_dashboard>ul>li:nth-child(9){display:none;}'; // @since v3.6.0 hide "Premium" submenu in its entirety; @modified v3.12.0 decrease with 1, due to Search Console submenu being removed; @since 3.14.8 include hiding "Workouts" submenu in its entirety @since 3.14.10 include hiding "Redirects" submenu in its entirety
+			echo 'li#toplevel_page_wpseo_dashboard>ul>li:nth-child(6),li#toplevel_page_wpseo_dashboard>ul>li:nth-child(7),li#toplevel_page_wpseo_dashboard>ul>li:nth-child(8){display:none;}'; // @since v3.6.0 hide "Premium" submenu in its entirety; @modified v3.12.0 decrease with 1, due to Search Console submenu being removed; @since 3.14.8 include hiding "Workouts" submenu in its entirety @since 3.14.10 include hiding "Redirects" submenu in its entirety; @since 3.14.13 Yoast SEO swapped menu items around
 		}
 
 		// hide Post/Page/Taxonomy Deletion Premium Ad
